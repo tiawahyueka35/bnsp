c123e1baaf09e4f127b3fae86ec6eafabc219e45
tiawahyueka35 <tiawahyueka35@gmail.com>
2021-10-26 21:08:33 +0700

    alhamdulillah

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+    include 'connection.php';
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Paper Dashboard 2 by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="./assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="white" data-active-color="danger">
+      <div class="logo">
+        <a href="https://www.creative-tim.com" class="simple-text logo-mini">
+          <!-- <div class="logo-image-small">
+            <img src="./assets/img/logo-small.png">
+          </div> -->
+          <!-- <p>CT</p> -->
+        </a>
+        <a href="https://www.creative-tim.com" class="simple-text logo-normal">
+          Menu
+          <!-- <div class="logo-image-big">
+            <img src="../assets/img/logo-big.png">
+          </div> -->
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="index.php?p=e-arsip">
+              <i class="fa fa-star mr-3"></i>
+              <p>Arsip</p>
+            </a>
+          </li>
+          <li>
+            <a href="index.php?p=about">
+              <i class="fa fa-exclamation-circle mr-3"></i>
+              <p>About</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel" style="height: 100vh;">
+        <!-- End of Sidebar -->
+        <?php
+            $pages_dir = 'pages';
+            if(!empty($_GET['p'])){
+                $pages = scandir($pages_dir,0);
+                unset($pages[0], $pages[1]);
+
+                $p = $_GET['p'];
+                if(in_array($p.'.php',$pages)){
+                    include($pages_dir.'/'.$p.'.php');
+                }else{
+                    echo'Halaman Tidak Ditemukan';
+                }
+            }else{
+                include($pages_dir.'/e-arsip.php');
+            }
+        ?>
+    <!--   Core JS Files   -->
+  <script src="./assets/js/core/jquery.min.js"></script>
+  <script src="./assets/js/core/popper.min.js"></script>
+  <script src="./assets/js/core/bootstrap.min.js"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="./assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="./assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ pages/about.php
@@ -0,0 +1,16 @@
+<div id="content" class="p-4 p-md-5 pt-5">
+    <div id="label-page"><h3><b>About</b></h3></div>
+    <div>
+        <table>
+            <tr>
+                <td><img src="./img/pasfoto.jpg" width=140px height=190px></td>
+                <td></td>
+                <td style="color: black">Aplikasi ini dibuat oleh:<br>
+                    Nama: Tia Wahyu Eka Ningsih<br>
+                    NIM: 1931710131<br>
+                    Tanggal: 26 Oktober 2021
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
